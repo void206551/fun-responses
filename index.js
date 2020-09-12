3b9578afb6fe7cc4ace8b524352438d0866609a0
the original void <32175146+void206551@users.noreply.github.com>
2020-09-12 16:49:37 -0400

    Something

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 const funny = './fun.json';
 
 // Joke call
-var {
-    joke
-} = require(funny);
-var resJoke = joke[Math.floor(Math.random() * joke.length)];
-
-exports.joke = resJoke
+exports.joke = function () {
+    var {
+        joke
+    } = require(funny);
+    var resJoke = joke[Math.floor(Math.random() * joke.length)];
+    return resJoke
+}
 
 // Pickup line call
-var {
-    pickup
-} = require(funny);
-var resPickup = joke[Math.floor(Math.random() * pickup.length)];
-
-exports.pickup = resPickup
+exports.pickup = function () {
+    var {
+        pickup
+    } = require(funny);
+    var resPick = pickup[Math.floor(Math.random() * pickup.length)];
+    return resPick
+}
 
 // Topic call
-var {
-    topic
-} = require(funny);
-var resTopic = topic[Math.floor(Math.random() * topic.length)];
-
-exports.topic = resTopic
+exports.topic = function () {
+    var {
+        topic
+    } = require(funny);
+    var resTopic = topic[Math.floor(Math.random() * topic.length)];
+    return resTopic
+}
 
 // Roast call
-var {
-    roast
-} = require(funny);
-var resRoast = roast[Math.floor(Math.random() * roast.length)];
-
-exports.roast = resRoast
+exports.roast = function () {
+    var {
+        roast
+    } = require(funny);
+    var resRoast = roast[Math.floor(Math.random() * roast.length)];
+    return resRoast
+}
 
 // Toast call
-var {
-    toast
-} = require(funny);
-var resToast = toast[Math.floor(Math.random() * toast.length)];
-
-exports.toast = resToast
+exports.toast = function () {
+    var {
+        toast
+    } = require(funny);
+    var resToast = toast[Math.floor(Math.random() * toast.length)];
+    return resToast
+}
