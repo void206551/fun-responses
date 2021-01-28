f7b2fdc7869ec6c60eb2b36f617f1e32f7d58ed4
Jason O'Donnell <32175146+void206551@users.noreply.github.com>
2021-01-27 20:03:32 -0500

    Update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const funny = './fun.json';
 
+const fuWeb = 'https://void206551.dev/projects/fun/fun.json'
+
 // Joke call
 exports.joke = function () {
     var {
         joke
-    } = require(funny);
+    } = require(fuWeb);
     var resJoke = joke[Math.floor(Math.random() * joke.length)];
     return resJoke
 }
@@ -13,7 +15,7 @@ exports.joke = function () {
 exports.pickup = function () {
     var {
         pickup
-    } = require(funny);
+    } = require(fuWeb);
     var resPick = pickup[Math.floor(Math.random() * pickup.length)];
     return resPick
 }
@@ -22,7 +24,7 @@ exports.pickup = function () {
 exports.topic = function () {
     var {
         topic
-    } = require(funny);
+    } = require(fuWeb);
     var resTopic = topic[Math.floor(Math.random() * topic.length)];
     return resTopic
 }
@@ -31,7 +33,7 @@ exports.topic = function () {
 exports.roast = function () {
     var {
         roast
-    } = require(funny);
+    } = require(fuWeb);
     var resRoast = roast[Math.floor(Math.random() * roast.length)];
     return resRoast
 }
@@ -40,7 +42,7 @@ exports.roast = function () {
 exports.toast = function () {
     var {
         toast
-    } = require(funny);
+    } = require(fuWeb);
     var resToast = toast[Math.floor(Math.random() * toast.length)];
     return resToast
 }
